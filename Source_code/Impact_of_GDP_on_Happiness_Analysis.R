64f6c23e36ba474e4f811f9e7f23c0c7d010cad6
sai kalyan <saikalyan@sriharis-MBP.lan>
2023-07-29 18:55:41 -0500

    Add happiness-1

--- Source_code/Impact_of_GDP_on_Happiness_Analysis.R
+++ Source_code/Impact_of_GDP_on_Happiness_Analysis.R
@@ -1,7 +1,8 @@
 
 
 # Load necessary libraries
-library(tidyverse)
+library(ggplot2)
+library(dplyr)
 
 
 # Read the dataset
@@ -9,21 +10,16 @@ happiness_data <- read.csv("Combined_Data_World_Happiness.csv")
 
 print(names(happiness_data))
 
+# Scatter plot for 2018 
+ggplot(happiness_data, aes(x = GDP.per.capita_2018, y = Happiness.Score_2018)) +
+  geom_point(color = 'blue', alpha = 0.5) +
+  labs(title = 'Impact of GDP on Happiness (2018)',
+       x = 'GDP per Capita',
+       y = 'Happiness Score')
 
-# Reshape the data
-happiness_data_melted_2018 <- happiness_data %>% select('Country', 'GDP.per.capita_2018', 'Happiness.Score_2018') %>% 
-  rename('GDP per capita' = 'GDP.per.capita_2018', 'Happiness Score' = 'Happiness.Score_2018') %>%
-  mutate(Year = '2018')
-
-happiness_data_melted_2019 <- happiness_data %>% select('Country', 'GDP.per.capita_2019', 'Happiness.Score_2019') %>% 
-  rename('GDP per capita' = 'GDP.per.capita_2019', 'Happiness Score' = 'Happiness.Score_2019') %>%
-  mutate(Year = '2019')
-
-happiness_data_melted <- rbind(happiness_data_melted_2018, happiness_data_melted_2019)
-
-# Create a box plot
-happiness_data_melted %>%
-  ggplot(aes(x=Year, y=`Happiness Score`, fill=Year)) +
-  geom_boxplot() +
-  labs(title="Box Plot of Happiness Score by Year", x="Year", y="Happiness Score") +
-  theme_minimal()
+# Scatter plot for 2019 
+ggplot(happiness_data, aes(x = GDP.per.capita_2019, y = Happiness.Score_2019)) +
+  geom_point(color = 'green', alpha = 0.5) +
+  labs(title = 'Impact of GDP on Happiness (2019)',
+       x = 'GDP per Capita',
+       y = 'Happiness Score')
